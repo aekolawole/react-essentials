2b5020d6b1bc0527b2d55aadda6bf1e73a6a0fb9
Adefemi Kolawole <adefemi.kolawole@hiscox.com>
2021-01-21 00:59:13 -0500

    develop: used props to inject dynamic data into page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
 
+function Header(props) {
+  console.log(props);
+  return <h1>Welcome to {props.name}'s Kitchen</h1>;
+}
+function Main(props) {
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around</p>
+    </section>
+  );
+}
+function Footer(props) {
+  return <p>&copy; Folakol Inc. {props.year}</p>;
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. My name is Olaitan.{" "}
-          <i>cant wait to meet my Faith</i>I
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Adewale"/>
+      <Main adjective="Tantalizing" />
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from './App';
+ import App from './App';
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  // React.createElement("h1", null, "Hey Mayowa"),
-  // document.getElementById("root")
-  // React.createElement(
-  //   "ul",
-  //   null,
-  //   React.createElement("li", null, "Monday"),
-  //   React.createElement("li", null, "Tuesday"),
-  //   React.createElement("li", null, "Wednesday")
-  // ),
-  <ul>
-    <li>Monday</li>
-    <li>Tuesday</li>
-    <li>Wednesday</li>
-  </ul>,
+ReactDOM.render(<App/>, 
   document.getElementById("root")
 );
 
